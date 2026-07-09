19c4fa74338493372173b9691167a54cfccf4479
Gaurav Sharma <gsharma9894@gmail.com>
2026-07-09 10:26:39 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/gaurav/autoap/AutoapApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav.autoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaurav/autoap/AutoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.autoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
